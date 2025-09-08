3d5b1dd58f436e9934ad16de4b8c00fe445e3648
Alex <92122843+Taduar2001@users.noreply.github.com>
2025-09-08 14:10:56 +0300

    update

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 System.register(["./application.js"], function (_export, _context) {
   "use strict";
 
-  var Application, canvas, $p, bcr, application;
+  var Application, canvas, application;
+  
   function topLevelImport(url) {
     return System["import"](url);
   }
+
   return {
-    setters: [function (_applicationJs) {
-      Application = _applicationJs.Application;
-    }],
+    setters: [
+      function (_applicationJs) {
+        Application = _applicationJs.Application;
+      }
+    ],
     execute: function () {
+      // Получаем Canvas
       canvas = document.getElementById('GameCanvas');
-      $p = canvas.parentElement;
-      bcr = $p.getBoundingClientRect();
-      canvas.width = bcr.width;
-      canvas.height = bcr.height;
+
+      // Фиксированные размеры 500x500
+      canvas.width = 500;
+      canvas.height = 500;
+
+      // Создаём экземпляр приложения
       application = new Application();
+
+      // Импорт движка Cocos Creator и инициализация приложения
       topLevelImport('cc').then(function (engine) {
         return application.init(engine);
       }).then(function () {
         return application.start();
-      })["catch"](function (err) {
+      }).then(function () {
+        // Отмечаем, что игра готова
+        window.GAME_READY = true;
+        console.log('Игра Cocos Creator готова и запущена');
+      }).catch(function (err) {
         console.error(err);
       });
     }
